17e594ffa5f2fee0efc165d8f2f1faba55df36f3
Sebastian Marines <sebastian0marines@gmail.com>
2020-04-04 13:37:09 -0600

    Add is_organization field to user model

--- racelens/users/models.py
+++ racelens/users/models.py
@@ -6,6 +6,7 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_organization = models.BooleanField(default=False)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
