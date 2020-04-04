00e881ba8f3294f1794efc2e6972040bdac6e4a7
Sebastian Marines <sebastian0marines@gmail.com>
2020-04-04 13:14:18 -0600

    Create homepage test

--- racelens/recognition/tests.py
+++ racelens/recognition/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class GeneralTest(TestCase):
+    def test_home_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
