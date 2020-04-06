a4c0c00776d5fd5c82392713bf9807ce1779df1a
Sebastian Marines <sebastian0marines@gmail.com>
2020-04-06 13:34:26 -0500

    Register models in admin site

--- racelens/recognition/admin.py
+++ racelens/recognition/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Photo, Selfie, Event
 
-# Register your models here.
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('imageId', 'event', 'user')
+
+
+class SelfieAdmin(admin.ModelAdmin):
+    list_display = ('user', 'id')
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('event_name', 'slug', 'user')
+    search_fields = ['event_name',]
+
+admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Selfie, SelfieAdmin)
+admin.site.register(Event, EventAdmin)
